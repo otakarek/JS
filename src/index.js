20ad42b6cbe713797f3ea4e6d8bd54abb8e8510c
otakarek <43402300+otakarek@users.noreply.github.com>
2018-10-17 11:19:36 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+document.getElementById("app").innerHTML = `Dobrý večer!`;
+/*
+alert('ahoj!');
+
+var x = 2;
+var z = x * y;
+var y = prompt("zadej cislo", "");
+
+document.getElementById("vysledek").innerHTML = "Násobek je " + (x * +y);
+
+document.getElementById("vysledek").innerHTML += '<br /><br />Rozdíl je ' + (x - (+y));
+var osmnact = confirm('Už Vám bylo 18?');
+alert(osmnact);
+
+function PrictiTri(x)
+{
+  return +x + 3;
+}
+
+alert(PrictiTri(cislo));
+*/
+//var cislo = prompt('Zadej cislo!', '');
+/*
+if(cislo >= 18)
+  alert('Už Vám bylo 18!');
+else
+  alert('Ještě Vám nebylo 18!');
+*/
+
+//alert((cislo >=18) ? 'Už Vám bylo 18!' : 'Ještě Vám nebylo 18!');
